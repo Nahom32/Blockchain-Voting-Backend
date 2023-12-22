3381a14ef2a0fb9409d637763a1de940a9c3ccfb
kalish93 <kaleabtekalign@gmail.com>
2023-12-22 11:16:19 +0300

    feat: added cors

--- express-backend/src/index.ts
+++ express-backend/src/index.ts
@@ -4,19 +4,19 @@ import authRoute from './api/auth/auth.route';
 import swaggerConfig from './swagger';
 import path from 'path';
 
-
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 app.use(express.static(path.join(__dirname, '../public')));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/user', userRoute);
-app.use('/api/auth', authRoute);
+app.use('/user', userRoute);
+app.use('/auth', authRoute);
 swaggerConfig(app); 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}/api/v1`);
   });
 
