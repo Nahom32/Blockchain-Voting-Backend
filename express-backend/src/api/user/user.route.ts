eea2f843a59bc082edf583a5a0b6b577083257a0
Kaleab Tekalign <91588962+kalish93@users.noreply.github.com>
2024-06-02 16:08:24 +0300

    feat: get users list (#34)

--- express-backend/src/api/user/user.route.ts
+++ express-backend/src/api/user/user.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createUserController, verifyEmailController} from "./user.controllers";
+import { createUserController, verifyEmailController, getUsersController} from "./user.controllers";
 
 const router = Router();
 
 router.post("/", createUserController);
 router.post("/verify-email", verifyEmailController);
+router.get("/", getUsersController);
 
 export default router;
---
+++ express-backend/src/index.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+import userRoute from './api/user/user.route';
+import authRoute from './api/auth/auth.route';
+import router from './api/file-upload/file-upload.route';
+import oraganizatinRoute from './api/organization/organizations.route'
+import swaggerConfig from './swagger';
+import path from 'path';
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+
+
+function isAllEnvSet() {
+  const envsName = [
+    'PORT',
+    'DATABASE_URL',
+    'ACCESS_TOKEN_SECRET_KEY',
+    'REFRESH_TOKEN_SECRET_KEY',
+    'ACCESS_TOKEN_EXPIRES_IN',
+    'REFRESH_TOKEN_EXPIRES_IN',
+
+    'SMTP_HOST',
+    'SMTP_PORT',
+    'SMTP_USERNAME',
+    'SMTP_PASSWORD',
+    'SMTP_SENDER',
+  ]
+
+  for (const envName of envsName) {
+    if (!process.env[envName]) {
+      console.error(`${envName} is not set in .env file`);
+      return false;
+    }
+  }
+  return true;
+}
+
+if (!isAllEnvSet()) {
+  process.exit(1);
+}
+  const app = express();
+  const port = process.env.PORT;
+  app.use(cors());
+  app.use(express.static(path.join(__dirname, '../public')));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser())
+  app.use(compress())
+  app.use(helmet())
+  app.use(cors())
+
+  app.use('/api/v1/user', userRoute);
+  app.use('/api/v1/auth', authRoute);
+  app.use('/api/v1/file', router);
+  app.use('/api/v1/oraganizatins', oraganizatinRoute);
+
+  swaggerConfig(app);
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Swagger is running at http://localhost:${port}/api-docs`);
+  });
+
