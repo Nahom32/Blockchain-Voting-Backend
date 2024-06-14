431581a35218fc3fe992f7621ede82c245fdfd9c
Kaleab Tekalign <91588962+kalish93@users.noreply.github.com>
2024-06-14 22:54:59 +0300

    fix: smart contract fix for getting organization elections (#45)

--- smart-contrats/migrations/2_deploy_contract.js
+++ smart-contrats/migrations/2_deploy_contract.js
@@ -1,9 +1,9 @@
 
 const ElectionVotingContract = artifacts.require("ElectionVotingContract");
-const ElectionStatisticsContract = artifacts.require("ElectionStatisticsContract");
+// const ElectionStatisticsContract = artifacts.require("ElectionStatisticsContract");
 
 module.exports = async function(deployer){
     await deployer.deploy(ElectionVotingContract);
     const electionVotingContractInstance = await ElectionVotingContract.deployed();
-    await deployer.deploy(ElectionStatisticsContract, electionVotingContractInstance.address);
+    // await deployer.deploy(ElectionStatisticsContract, electionVotingContractInstance.address);
 }
